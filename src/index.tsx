531867079644c29de468f1fabab7b00c9d3b275b
south <fancustard9@gmail.com>
2024-08-31 17:50:47 +0900

    Reactの型について学んだ

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+//import App from './App';
+import reportWebVitals from "./reportWebVitals";
+//import Hello from "./components/Hello";
+//import Name from "./components/Name";
+import Message from "./components/Message";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Hello />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+const container = document.getElementById("root");
+if (container) {
+  const root = ReactDOM.createRoot(container);
+  root.render(
+    <React.StrictMode>
+      {/* <Hello /> */}
+      {/* <Name /> */}
+      <Message />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
